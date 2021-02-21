e530888b1f28d95046c36f69c5dadd710a0b02bc
Pedro Satin <pedro5satin@gmail.com>
2021-02-21 12:30:48 -0300

    Creating query to get users

--- src/types/Query.ts
+++ src/types/Query.ts
@@ -60,5 +60,13 @@ export const Query = queryType({
         })
       },
     })
+
+    t.list.field('users', {
+      type: 'User',
+      resolve: (parent, args, ctx) => {
+        return ctx.prisma.user.findMany()
+      },
+    })
+
   },
 })
